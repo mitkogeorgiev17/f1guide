54d517fe543222d687990417fef6f4093b2bf8c6
Mitko <mgeorgiev317@gmail.com>
2023-06-01 01:34:00 +0300

    lightsOutGame

--- index.js
+++ index.js
@@ -1,39 +1,61 @@
-start();
+const clickArea = $(".click-area");
+const displayText = $(".display-text");
+const scoreElements = $(".score");
+const pbScore = 200000;
 
-function start(){
-    $("#reaction").html("Your reaction time: ");
-    var randomTime = Math.floor(Math.random()*4000);
+const scoreHistory = [];
 
-    $(".f1-lights").click(function(){
-        lightsOn();
-        setTimeout(function(){
-            $(".f1light").css("background-color","rgb(41,41,41)");
-            var startTime = Date.now();
-            $(".f1-lights").click(function(){
-                $(".f1-lights").off('click');
-                var endTime = Date.now();
-                var reactionTime = endTime - startTime;
-                $("#reaction").html("Your reaction time: " + reactionTime/1000 + " seconds.");
-                $(".f1-lights").click(function(){
-                    start();
-                });
-            });
-        },4000+randomTime);
-    })
-}
+const MIN_CHANGE = 5700;
+const MAX_CHANGE = 10000;
 
-function lightsOn(){
-    $(".one #lightable").css("background-color","red");
+let startTime = 0;
+let waitingForClick = false;
+
+function play(){
+    let msTillChange = Math.floor(Math.random() * (MAX_CHANGE-MIN_CHANGE)) + MIN_CHANGE;
+
+    $(".one .lightable").addClass("light-on");
     setTimeout(function(){
-        $(".two #lightable").css("background-color","red");
+        $(".two .lightable").addClass("light-on");
         setTimeout(function(){
-            $(".three #lightable").css("background-color","red");
+            $(".three .lightable").addClass("light-on");
             setTimeout(function(){
-                $(".four #lightable").css("background-color","red");
+                $(".four .lightable").addClass("light-on");
                 setTimeout(function(){
-                    $(".five #lightable").css("background-color","red");
+                    $(".five .lightable").addClass("light-on");
                 },1000);
             },1000);
         },1000);
     },1000);
+    $(".display-text").html("...");
+
+    setTimeout(function(){
+        startTime = Date.now();
+
+        $(".lightable").removeClass("light-on");
+        waitingForClick = true;
+    },msTillChange);
 }
+
+function addScore(score){
+    scoreHistory.unshift(score);
+
+    for (let i = 0; i < Math.min(scoreHistory.length, 5); i++) {
+        const score = scoreHistory[i];
+
+        scoreElements[i].textContent = `${score} seconds.`;
+    }
+}
+
+$(".click-area").click(function(){
+    if (waitingForClick){
+        const score = (Date.now() - startTime) / 1000;
+        
+        waitingForClick = false;
+        $(".display-text").html("Your time was " + score + " ms. Click to play again.");
+
+        addScore(score);
+    } else {
+        play();
+    }
+});
